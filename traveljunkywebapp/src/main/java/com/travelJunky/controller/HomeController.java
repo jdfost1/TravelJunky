4590feae40939bb3ccffcb2204e3012c5eac4a26
Foster, Josh <jdfost1@ilstu.edu>
2019-10-03 08:42:11 -0500

    comments and clean code

--- traveljunkywebapp/src/main/java/com/travelJunky/controller/HomeController.java
+++ traveljunkywebapp/src/main/java/com/travelJunky/controller/HomeController.java
@@ -1,32 +1,16 @@
 package com.travelJunky.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
-import com.travelJunky.entities.User;
-import com.travelJunky.service.UserService;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private UserService userService;
+
 
 	@RequestMapping("/")
 	public String showHomePage(Model model) {
-		String email = SecurityContextHolder.getContext().getAuthentication().getName();
-		if (email.contains("@")) {
-			User user = userService.findByEmail(email);
-			if (user != null) {
-//				Budget budget = user.getBudget();
-//				model.addAttribute("budget", budget);
-			}
-		}
+
 		return "index";
 	}
 
